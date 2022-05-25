827ecd4db43ebb3e2b900d7a5b4dc36c163563f5
Vladimir <vladimirhoris@gmail.com>
2022-05-25 22:51:15 +0700

    add new fields

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 
 
-class Catigories(models.Model):
+class Categories(models.Model):
     name = models.CharField('Название категории', max_length=25, db_index=True, )
     image = models.ImageField('Изображение', upload_to='image/catigories/%Y/%m/%d')
     url = models.SlugField(max_length=52, db_index=True, blank=True, )
@@ -10,28 +11,33 @@ class Catigories(models.Model):
     def save(self, *args, ):
         if not self.id:
             self.url = slugify(self.name)
-        super(Catigories, self).save(*args)
+        super(Categories, self).save(*args)
+
+    def get_absolute_url(self):
+        return reverse('scat', kwargs={'cat_slug': self.url})
 
     def __str__(self):
         return f'{self.name}'
 
 
 class Products(models.Model):
-    name = models.CharField('Название', max_length=100, db_index=True)
+    name_model = models.CharField('Название', max_length=100, db_index=True)
     discription = models.TextField('Описание', max_length=1000, db_index=True)
     price = models.IntegerField('Цена', )
-    main_image = models.ImageField('Главное изображение', upload_to='image/products/%Y/%m/%d',)
-    category = models.ForeignKey(Catigories, on_delete=models.CASCADE, )
-    url = models.SlugField(max_length=52, db_index=True, blank=True)
+    main_image = models.ImageField('Главное изображение', upload_to='image/products/%Y/%m/%d', )
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE, )
+    brand = models.CharField('Производитель', max_length=20, db_index=True, blank=True)
+    discount = models.BooleanField(default=False)
     available = models.BooleanField(default=True)
+    url = models.SlugField(max_length=52, db_index=True, blank=True)
 
     def save(self, *args, ):
         if not self.id:
-            self.url = slugify(self.name)
+            self.url = slugify(self.name_model)
         super(Products, self).save(*args)
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.name_model}'
 
 
 class Image(models.Model):
