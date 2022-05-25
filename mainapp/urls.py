3be66173a5d5de1411a94be65c5a9462bb4e240d
Vladimir <vladimirhoris@gmail.com>
2022-05-25 22:51:57 +0700

    add url filters

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import MainView
+from .views import MainView, SelectCatView, ProductsView
 
 urlpatterns = [
-    path('', MainView.as_view(), name='main')
+    path('', MainView.as_view(), name='main'),
+    path('products/', SelectCatView.as_view(), name='filter_prod'),
 ]
